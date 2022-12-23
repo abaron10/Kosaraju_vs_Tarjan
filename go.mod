ca7e9cf0617a906b0efaee92e206de8d81ee0c0f
andres.baron <andres.baron@rappi.com>
2022-12-23 09:44:35 -0500

    kosaraju base added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module SCC_analysis
+
+go 1.18
+
+require github.com/abaron10/Gothon v0.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+
+	//d := graph.NewGraph()
+	/*	d.AddEdge(0, 1)
+		d.AddEdge(1, 2)
+		d.AddEdge(2, 3)
+		d.AddEdge(3, 0)
+		d.AddEdge(2, 4)
+		d.AddEdge(4, 5)
+		d.AddEdge(5, 6)
+		d.AddEdge(6, 4)
+		d.AddEdge(7, 6)
+		d.AddEdge(7, 8)*/
+
+	/*	d.AddEdge(0, 1)
+		d.AddEdge(0, 5)
+		d.AddEdge(3, 5)
+		d.AddEdge(3, 2)
+		d.AddEdge(2, 0)
+		d.AddEdge(2, 3)
+		d.AddEdge(3, 3)
+		d.AddEdge(5, 4)
+		d.AddEdge(4, 3)
+		d.AddEdge(4, 2)
+		d.AddEdge(6, 4)
+		d.AddEdge(6, 0)
+		d.AddEdge(6, 8)
+		d.AddEdge(8, 6)
+		d.AddEdge(7, 6)
+		d.AddEdge(6, 9)
+		d.AddEdge(7, 9)
+		d.AddEdge(11, 4)
+		d.AddEdge(9, 11)
+		d.AddEdge(9, 10)
+		d.AddEdge(10, 12)
+		d.AddEdge(12, 9)
+		d.AddEdge(11, 12)
+
+		d.EvaluateKosaraju()*/
+
+}
